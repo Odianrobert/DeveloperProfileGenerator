b0f3e7c97aeff9ded66f85209958e7eb6277cc65
Bobby Odian <odianrobert@yahoo.com>
2020-01-26 13:36:35 -0500

    complete

--- index.js
+++ index.js
@@ -44,10 +44,27 @@ function gitCall(answers) {
   })
 }
 
+function starred(answers) {
+  let name = answers.name
+  axios.get(`https://api.github.com/users/${name}/starred`
+  )
+  .then(function (response,) {
+    let star = response.data.full_name
+    // let starRepos = response.data;
+    //  const starRepos1 = starRepos.full_name.length
+    // let star = response.data.full_name;
+    // const starRepos1 = starRepos.full_name;
+     console.log(star)
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+}
+
 inquirer.prompt(questions)
   .then(answers => {
     gitCall(answers)
-    //createPDF()
+    starred(answers)
     user=answers
   })
   .catch(function(err) {
@@ -69,7 +86,7 @@ function generateHTML(response) {
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
   <title>Document</title>
   <style>
-body {background-color:${user.color};
+body {
 height:1300px;
 }
 </style>
@@ -80,21 +97,22 @@ height:1300px;
   
   <div class="container"><br>
   <center>
-      <div style="background-color: lightgray; border: black 5px solid; width: 350px; height: 375px; margin-bottom: 50px;">
+      <div style="background-color:${user.color}; border: black 5px solid; width: 400px; height: 450px; margin-bottom: 50px;">
         <h1 style="text-align:center;">${response.data.name}</h1>
-        <img src="${response.data.avatar_url}" align="middle" alt="user profile picture" height="300" width="300">
+        <img src="${response.data.avatar_url}" align="middle" alt="user profile picture" height="350" width="300">
       </div>
   </center>
   
 
-  <ul style="background-color: lightgray; border: black 5px solid;"> 
+  <ul style="background-color:${user.color}; border: black 5px solid;"> 
     <li class="lead">Bio: ${response.data.bio}</li>
     <li class="lead">I am following: ${response.data.following} GitHub Users</li>
     <li class="lead">My number of public repositories: ${response.data.public_repos}</li>
     <li class="lead">My number of followers: ${response.data.followers}</li>
+    <li class="lead">My starred Repos: ${response.data.full_name}</li>
   </ul>
 
-  <div style="background-color: lightgray; border: black 5px solid; margin-bottom: 50px;">
+  <div style="background-color:${user.color}; border: black 5px solid; margin-bottom: 50px;">
     <h2>Links<h2>
       <ul>
         <li class="lead">GitHub: ${response.data.html_url}</li>
@@ -103,9 +121,9 @@ height:1300px;
   </div>
 
   <center>
-    <div  style="background-color: lightgray; border: black 5px solid; width:650px; height:275px; margin-bottom: 50px;">
+    <div  style="background-color:${user.color}; border: black 5px solid; width:650px; height:350px; margin-bottom: 50px;">
       <h5>Current city ${response.data.location}</h5>
-      <img src="https://maps.googleapis.com/maps/api/staticmap?center=${uLocation}&zoom=14&size=600x200&key=AIzaSyDIEVzD85LZ_BWwmWAD2qPxTiUNGgA28YI" align="middle">
+      <img src="https://maps.googleapis.com/maps/api/staticmap?center=${uLocation}&zoom=14&size=600x300&key=AIzaSyDIEVzD85LZ_BWwmWAD2qPxTiUNGgA28YI" align="middle">
     </div>
   <center>
 
@@ -115,7 +133,6 @@ height:1300px;
 </html>`;
 
 }
-
 
 
 
